7a634e7752fb0f26bad543bf6bfd38e9e7d36285
ikhwanpermana <ikhwanpermana1@gmail.com>
2026-05-18 13:54:26 +0000

    config: mengaktifkan adapter vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless'; // Wajib diimpor
 
 export default defineConfig({
+  // Menghubungkan Tailwind CSS agar utility class berfungsi
+  integrations: [tailwind()],
+  
+  // Mengaktifkan adapter Vercel Serverless agar fungsi database SSR berjalan
   output: 'server',
   adapter: vercel(),
 });
