5974128a5d77944cc03511e1f153fa4b36be2574
ikhwanpermana <ikhwanpermana1@gmail.com>
2026-05-19 09:42:53 +0000

    fix: hapus integrasi tailwind lama karena beralih ke cdn v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  // 1. Mengaktifkan mode SSR
-  output: 'server', 
-  // 2. Menggunakan adaptor Vercel agar server berjalan di cloud
-  adapter: vercel(),
+  output: 'server',
+  adapter: vercel()
 });
