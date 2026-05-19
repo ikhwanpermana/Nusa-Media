1f370689bac4646e901095e02dd9d7930fc87355
ikhwanpermana <ikhwanpermana1@gmail.com>
2026-05-19 02:34:08 +0000

    chore: remove env file from tracking and update gitignore

---
+++ src/lib/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+import type { Database } from '../../types/database.types'
+
+// Mengambil variabel environment bawaan Astro
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
