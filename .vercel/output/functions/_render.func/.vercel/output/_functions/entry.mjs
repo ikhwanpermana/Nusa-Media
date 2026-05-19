9df77ea589f320b5c697a666f96c68064847a104
ikhwanpermana <ikhwanpermana1@gmail.com>
2026-05-19 10:43:00 +0000

    fix: update node version and vercel analytics

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_B318OQwv.mjs';
-import { manifest } from './manifest_BXYjlbpW.mjs';
+import { manifest } from './manifest_B_SI_Ru4.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/admin.astro.mjs');
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "ebc4b34b-cc00-4892-b00d-b1837cd97419",
+    "middlewareSecret": "98f74202-bf4d-4189-b08e-8986d6bd2263",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
