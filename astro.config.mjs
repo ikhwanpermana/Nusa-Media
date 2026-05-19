310e529960886064e976c73aabe9201fb2fe8c73
ikhwanpermana <ikhwanpermana1@gmail.com>
2026-05-19 06:45:15 +0000

    fix: nonaktifkan gpg dan set ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',         // Mengubah mode dari statis ke server
-  adapter: vercel(),        // Memberitahu Astro untuk menggunakan mesin Vercel
+  // 1. Mengaktifkan mode SSR
+  output: 'server', 
+  // 2. Menggunakan adaptor Vercel agar server berjalan di cloud
+  adapter: vercel(),
 });
