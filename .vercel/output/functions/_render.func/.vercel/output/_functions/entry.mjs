c0409ef77056a3f43afffbcae8406a90b6f9f748
ikhwanpermana <ikhwanpermana1@gmail.com>
2026-05-19 10:24:33 +0000

    rapi

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_B318OQwv.mjs';
-import { manifest } from './manifest_BOzpxzXT.mjs';
+import { manifest } from './manifest_BXYjlbpW.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/admin.astro.mjs');
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "e8ede384-1305-4bfa-aa82-87f5d34f6f5a",
+    "middlewareSecret": "ebc4b34b-cc00-4892-b00d-b1837cd97419",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
