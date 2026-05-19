3000a08e366c399f19d5089f5ff2dcce438b0297
ikhwanpermana <ikhwanpermana1@gmail.com>
2026-05-19 10:50:31 +0000

    chore: remove local vercel output and update gitignore

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/supabase_ydpuITTZ.mjs
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = undefined                                   ;
-const supabaseAnonKey = undefined                                        ;
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-export { supabase as s };
