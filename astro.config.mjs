c55fe7e932958160adb69237ce73dcc0f882ec9e
ikhwanpermana <email_github_anda@example.com>
2026-05-20 20:52:04 +0900

    fix: gunakan konfigurasi runtime resmi astro vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
-  vite: {
-    build: {
-      target: 'node18' // 🎯 Trik mengelabui compiler
-    }
-  }
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    // 🎯 Trik Utama: Memaksa Vercel menggunakan runtime Node 22 secara resmi via Astro
+    functionPerRoute: false,
+    edgeMiddleware: false,
+    runtime: 'nodejs22.x' 
+  }),
 });
