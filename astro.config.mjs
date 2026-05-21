ad69e3abe033727029c98e75d25a4056bfa821e3
ikhwanpermana <email_github_anda@example.com>
2026-05-21 22:50:54 +0900

    fix: memperbaiki typo ekstensi astro dan mengaktifkan mode hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel'; // Gunakan paket vercel standar terbaru
+import vercel from '@astrojs/vercel';
 
-// https://astro.build
+// Konfigurasi resmi Nusa Media untuk Vercel + Supabase
 export default defineConfig({
-  // Kita gunakan mode 'hybrid' agar halaman beranda Anda tetap statis (cepat), 
-  // tetapi halaman admin rahasia bisa berjalan secara dinamis di server Vercel.
   output: 'hybrid', 
-  
   adapter: vercel({
     webAnalytics: {
       enabled: true 
