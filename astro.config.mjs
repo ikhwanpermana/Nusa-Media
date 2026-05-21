d633ac878053bd0c3d4f5efaf3bd373544cd3520
ikhwanpermana <email_github_anda@example.com>
2026-05-21 22:47:25 +0900

    chore: aktifkan mode hybrid dan adapter vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// import vercel from '@astrojs/vercel/serverless'; // ⏸️ DI-NONAKTIFKAN SEMENTARA
+import vercel from '@astrojs/vercel'; // Gunakan paket vercel standar terbaru
 
+// https://astro.build
 export default defineConfig({
-  output: 'static', // ⚙️ Diubah ke statis agar lolos dari error Node.js Vercel
-  /* ⏸️ Menonaktifkan sementara adaptor engine serverless Vercel
+  // Kita gunakan mode 'hybrid' agar halaman beranda Anda tetap statis (cepat), 
+  // tetapi halaman admin rahasia bisa berjalan secara dinamis di server Vercel.
+  output: 'hybrid', 
+  
   adapter: vercel({
     webAnalytics: {
       enabled: true 
     }
   }),
-  */
   integrations: [tailwind()],
 });
